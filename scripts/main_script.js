6f3a3a00c9c2d2a195cbed69fb613b528a4ae65c
Verachill <sayfulloev0472@gmail.com>
2020-05-30 10:54:58 +0300

    Fixed main page
    Finally completed main page, but have no link's to another pages.

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -4,12 +4,18 @@ window.onload = function(){
 
 function headerAnimation () {
 	if (window.innerWidth > 992){
-		expandAnim()
+		expandAnim();
+		catName = document.getElementsByClassName('categories-name');
+		fontSize(catName);
 	}
 	else{
-		expandAnimMobile()
+		expandAnimMobile();
+		choiseLink = document.getElementsByClassName('a-full')[0];
+		choiseLink.setAttribute('id', 'a-full')
 	}
 }
+
+//menu
 
 function expandAnim () {
 	var header = document.getElementsByClassName('header')[0];
@@ -20,7 +26,7 @@ function expandAnim () {
 	function scrollUpdate () {
 		var coord = pageYOffset;
 
-		if (coord > 700 && isOpen == false){
+		if (coord > 991 && isOpen == false){
 			headerBack.style.height = '60px';
 		} 
 		else{
@@ -52,18 +58,64 @@ function expandAnim () {
 	});
 }
 
+//menu mobile
+
 function expandAnimMobile () {
 	var button = document.getElementById('menu');
 		header = document.getElementsByClassName('header')[0];
-		isOpen = false;
-		button.addEventListener('click', function(){
-			if (isOpen == false){
-				isOpen = true;
-				header.classList.add('mobile');
+		headerOverlay = document.getElementsByClassName('header-overlay')[0];
+		svg = document.getElementsByClassName('header-icon');
+		svgMain = document.getElementsByClassName('a-icon');
+	button.addEventListener('click', function(){
+		if (header.id != 'mobile'){              //with open
+			header.setAttribute('id', 'mobile');
+			headerOverlay.setAttribute('id', 'header-overlay');
+			for (var i = 0; i < svg.length; i++){
+				svg[i].setAttribute('fill', '#1C1C1C');
+				svgMain[i].style.display = 'block';
 			}
-			else{
-				isOpen = false;
-				header.classList.remove('mobile');
+			onClickClose(header);
+		}
+		else{              //with close
+			header.removeAttribute('id', 'mobile');
+			headerOverlay.removeAttribute('id', 'header-overlay');
+			for (var i = 0; i < svg.length; i++){
+				svgMain[i].style.display  = 'none';
 			}
-		})
+		}
+	})
+}
+
+//Close menu
+
+function onClickClose(elem) { // вызвать в момент показа окна, где elem - окно
+    function outsideClickListener(event) {
+        if (!elem.contains(event.target)) {  // проверяем, что клик не по элементу
+            elem.removeAttribute('id', 'mobile'); //скрыть
+            headerOverlay.removeAttribute('id', 'header-overlay'); //скрыть
+			for (var i = 0; i < svg.length; i++){
+				svgMain[i].style.display  = 'none';
+			}
+            document.removeEventListener('click', outsideClickListener);
+        }
+    }
+    document.addEventListener('click', outsideClickListener);
+}
+
+//font size from word's lenght
+
+function fontSize(h1) {
+	var f = 0;
+	for (var i = 0; i < 4; i++){
+		if (h1[i].innerHTML.length > 20)
+			f = 2
+		else if (h1[i].innerHTML.length > 10)
+			f = 1
+	}
+	for (var i = 0; i < 4; i++){
+		if (f == 1)
+			h1[i].style.fontSize = '32px';
+		else if (f == 2)
+			h1[i].style.fontSize = '26px';
+	}
 }
