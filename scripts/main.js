3e65a8ac9c3415ab997cfeafbf28d6f49acda1f4
Verachill <sayfulloev0472@gmail.com>
2020-06-05 19:51:19 +0300

    Ne, nu this is FETCH

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
+
+	commentsColor();
+	commentOpenEnable();
+
 	if (window.innerWidth > 992){
 		catName = document.getElementsByClassName('categories-name');
 		fontSize(catName);
@@ -19,4 +23,28 @@ function fontSize(h1) {
 		else if (f == 2)
 			h1[i].style.fontSize = '26px';
 	}
+};
+
+function commentsColor() {
+	var comments = document.getElementsByClassName('comment'),
+		commentBorder = document.getElementsByClassName('comment-text');
+
+	console.log(comments)
+	for(var i = 1; i < comments.length; i += 2){
+		comments[i].style.color = 'rgba(230, 128, 56, .95)';
+		commentBorder[i].style.borderLeft = '1px solid rgba(247, 151, 83, .6)'
+	};
+};
+
+function commentOpenEnable() {
+	var text = document.getElementsByClassName('write-comment')[0],
+		btn = document.getElementById('write-comment-btn');
+
+	btn.addEventListener('click', function(){
+		text.classList.add('write-comment-open');
+	});
+
+	btn.removeEventListener('click', function(){
+		text.classList.add('write-comment-open');
+	});
 };
